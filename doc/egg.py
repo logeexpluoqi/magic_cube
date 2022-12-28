ec336a1b580a9cd27121cbef1cd6c2ad925dfb60
luoqi <logeexpluoqi@outlook.com>
2022-12-28 21:54:49 +0800

    code(*): change some code

--- doc/egg.py
+++ doc/egg.py
@@ -8,7 +8,7 @@ ax = fig.add_subplot(projection = '3d')
 [x, t] = np.meshgrid(np.array(range(25)) / 24.0,
                      np.arange(0, 575.5, 0.5) / 575 * 20 * np.pi + 4*np.pi)
 p = (np.pi / 2) * np.exp(-t / (8 * np.pi))
-change = np.sin(15*t)/150
+change = np.sin(15*t)/1500
 u = 1 - (1 - np.mod(3.3 * t, 2 * np.pi) / np.pi) ** 4 / 2 + change
 y = 2 * (x ** 2 - x) ** 2 * np.sin(p)
 r = u * (x * np.sin(p) + y * np.cos(p))

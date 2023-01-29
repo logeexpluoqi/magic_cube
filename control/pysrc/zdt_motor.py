1c9be578299d429a93d3ef0280c29b723eb80eff
luoqi <logeexpluoqi@outlook.com>
2023-01-29 21:05:08 +0800

    fix(pysrc): fixed recv error

--- control/pysrc/zdt_motor.py
+++ control/pysrc/zdt_motor.py
@@ -27,7 +27,10 @@ class ZdtMotor(serial.Serial):
         self.write(data)
 
     def _recv(self, size):
-        return self.read(size)
+        try:
+            return self.read(size)
+        except:
+            print(" recv error !")
 
     def ischeck(self, state):
         self.ischeck = state
@@ -67,71 +70,101 @@ class ZdtMotor(serial.Serial):
         
     def read_enc(self):
         self._send(bytes([self.id, 0x30, 0x6b]))
-        data = self._recv(4)
-        self.enc = (int(data[1]) << 8) | data[2]
+        try:
+            data = self._recv(4)
+            self.enc = (int(data[1]) << 8) | data[2]
+        except:
+            print(" recv error !")
         
     def read_pul(self):
         self._send(bytes([self.id, 0x33, 0x6b]))
-        data = self._recv(6)
-        self.pul = (int(data[1]) << 24) | (int(data[2]) << 16) | (int(data[3]) << 8) | data[4]
-        
+        try:
+            data = self._recv(6)
+            self.pul = (int(data[1]) << 24) | (int(data[2]) << 16) | (int(data[3]) << 8) | data[4]
+        except:
+           print(" recv error !")
+            
     def read_pos(self):
         self._send(bytes([self.id, 0x36, 0x6b]))
-        data = self._recv(6)
-        self.pos = (int(data[1]) << 24) | (int(data[2]) << 16) | (int(data[3]) << 8) | data[4]
+        try:
+            data = self._recv(6)
+            self.pos = (int(data[1]) << 24) | (int(data[2]) << 16) | (int(data[3]) << 8) | data[4]
+        except:
+            print(" recv error !")
         
     def read_perr(self):
         self._send(bytes([self.id, 0x39, 0x6b]))
-        data = self._recv(4)
-        self.perr = (int(data[1]) << 24) | data[2]
+        try:
+            data = self._recv(4)
+            self.perr = (int(data[1]) << 24) | data[2]
+        except:
+            print(" recv error !")
         
     def read_ena(self):
         self._send(bytes([self.id, 0x3a, 0x6b]))
-        data = self._recv(3)
-        self.ena = data[1]
+        try:
+            data = self._recv(3)
+            self.ena = data[1]
+        except:
+            print(" recv error !")
         
     def read_lock(self):
         self._send(bytes([self.id, 0x3e, 0x6b]))
-        data = self._recv(3)
-        self.lock = data[1]
+        try:
+            data = self._recv(3)
+            self.lock = data[1]
+        except:
+            print(" recv error !")
         
     def read_auto_zero(self):
         self._send(bytes([self.id, 0x3f, 0x6b]))
-        data = self._recv(3)
-        self.zero_state = data[1]
+        try:
+            data = self._recv(3)
+            self.zero_state = data[1]
+        except:
+            print(" recv error !")
         
     def div_set(self, div):
         self._send(bytes([self.id, 0x84, div, 0x6b]))
         if (self.ischeck == True):
-            buf = self._recv(3)
-            if buf[1] == 0x02:
-                self.err = 0
-            elif buf[1] == 0xee:
-                self.err = 0xee
-            else:
-                self.err = 0
+            try:
+                buf = self._recv(3)
+                if buf[1] == 0x02:
+                    self.err = 0
+                elif buf[1] == 0xee:
+                    self.err = 0xee
+                else:
+                    self.err = 0
+            except:
+                print(" recv error !")
         
-    def id_set(self):
+    def id_set(self, id):
         self._send(bytes([self.id, 0xae, id, 0x6b]))
         if (self.ischeck == True):
-            buf = self._recv(3)
-            if buf[1] == 0x02:
-                self.err = 0
-            elif buf[1] == 0xee:
-                self.err = 0xee
-            else:
-                self.err = 0
+            try:
+                buf = self._recv(3)
+                if buf[1] == 0x02:
+                    self.err = 0
+                elif buf[1] == 0xee:
+                    self.err = 0xee
+                else:
+                    self.err = 0
+            except:
+                print(" recv error !")
         
     def ena_set(self, state):
         self._send(bytes([self.id, 0xf3, state, 0x6b]))
         if (self.ischeck == True):
-            buf = self._recv(3)
-            if buf[1] == 0x02:
-                self.err = 0
-            elif buf[1] == 0xee:
-                self.err = 0xee
-            else:
-                self.err = 0
+            try:
+                buf = self._recv(3)
+                if buf[1] == 0x02:
+                    self.err = 0
+                elif buf[1] == 0xee:
+                    self.err = 0xee
+                else:
+                    self.err = 0
+            except:
+                print(" recv error !")
         
     def vel_set(self, vel, acc):
         if (vel > 1279):
@@ -149,13 +182,16 @@ class ZdtMotor(serial.Serial):
         
         self._send(bytes([self.id, 0xf6, (vel >> 8) & 0xff, vel & 0xff, acc, 0x6b]))
         if (self.ischeck == True):
-            buf = self._recv(3)
-            if buf[1] == 0x02:
-                self.err = 0
-            elif buf[1] == 0xee:
-                self.err = 0xee
-            else:
-                self.err = 0
+            try:
+                buf = self._recv(3)
+                if buf[1] == 0x02:
+                    self.err = 0
+                elif buf[1] == 0xee:
+                    self.err = 0xee
+                else:
+                    self.err = 0
+            except:
+                print(" recv error !")
     
     def pos_set(self, pos, vel, acc):
         if (vel > 1279):
@@ -173,13 +209,16 @@ class ZdtMotor(serial.Serial):
             
         self._send(bytes([self.id, 0xfd, (vel >> 8) & 0xff, vel & 0xff, acc, (pos >> 16) & 0xff, (pos >> 8) & 0xff, pos & 0xff, 0x6b]))
         if (self.ischeck == True):
-            buf = self._recv(3)
-            if buf[1] == 0x02:
-                self.err = 0
-            elif buf[1] == 0xee:
-                self.err = 0xee
-            else:
-                self.err = 0
+            try:
+                buf = self._recv(3)
+                if buf[1] == 0x02:
+                    self.err = 0
+                elif buf[1] == 0xee:
+                    self.err = 0xee
+                else:
+                    self.err = 0
+            except:
+                print(" recv error !")
                 
     def param_state(self, state):
         if state == "clear":
@@ -188,11 +227,14 @@ class ZdtMotor(serial.Serial):
             cmd = 0xc8
         self._send(bytes([self.id, 0xff, cmd,0x6b]))
         if (self.ischeck == True):
-            buf = self._recv(3)
-            if buf[1] == 0x02:
-                self.err = 0
-            elif buf[1] == 0xee:
-                self.err = 0xee
-            else:
-                self.err = 0
+            try:
+                buf = self._recv(3)
+                if buf[1] == 0x02:
+                    self.err = 0
+                elif buf[1] == 0xee:
+                    self.err = 0xee
+                else:
+                    self.err = 0
+            except:
+                print(" recv error !")
         

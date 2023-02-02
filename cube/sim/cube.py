66dce1b904cecebb225fa2fce676fbd134b57a1d
luoqi <logeexpluoqi@outlook.com>
2023-02-03 01:01:25 +0800

    code(cube): add cube face distribute array and is color

--- cube/sim/cube.py
+++ cube/sim/cube.py
@@ -1,12 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 
 class Cube:
     def __init__(self, order:int):
         self.order = order
         self.color = {"y": "#ffe600", "w": "#fffffb", "o": "#f15a20", "r": "#ed1941", "b": "#2a5caa", "g": "#65c294", "bc": "#afb4db"}
+        self.face_color = {"y": 0, "w": 1, "r": 2, "o": 3, "b": 4, "g": 5}
+        self.face_up    = np.zeros((self.order, self.order), dtype = int)
+        self.face_down  = np.zeros((self.order, self.order), dtype = int)
+        self.face_left  = np.zeros((self.order, self.order), dtype = int)
+        self.face_right = np.zeros((self.order, self.order), dtype = int)
+        self.face_front = np.zeros((self.order, self.order), dtype = int)
+        self.face_back  = np.zeros((self.order, self.order), dtype = int)
         
+        for i in range(self.order):
+            for j in range(self.order):
+                self.face_up[i, j]    = self.face_color["y"]
+                self.face_down[i, j]  = self.face_color["w"]
+                self.face_left[i, j]  = self.face_color["r"]
+                self.face_right[i, j] = self.face_color["o"]
+                self.face_front[i, j] = self.face_color["b"]
+                self.face_back[i, j]  = self.face_color["g"]
+                
         # prepare some axis points
         self.n_voxels = np.ones((self.order + 2, self.order + 2, self.order + 2), dtype = bool)
         self.size = np.array(self.n_voxels.shape) * 2
@@ -44,22 +59,12 @@ class Cube:
         self.body[-1, :, 0]  = 0
         self.body[-1, :, -1] = 0
         
-        self.faces_up    = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
-        self.faces_down  = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
-        self.faces_left  = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
-        self.faces_right = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
-        self.faces_front = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
-        self.faces_back  = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
-        
-        # for i in range(self.order * 2 + 3):
-        #     # i //= 2
-        
-        self.faces_up[:, :]    = self.color["y"]
-        self.faces_down[:, :]  = self.color["w"]
-        self.faces_right[:, :] = self.color["o"]
-        self.faces_left[:, :]  = self.color["r"]
-        self.faces_front[:, :] = self.color["b"]
-        self.faces_back[:, :]  = self.color["g"]
+        self.colors_up    = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
+        self.colors_down  = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
+        self.colors_left  = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
+        self.colors_right = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
+        self.colors_front = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
+        self.colors_back  = np.full((self.order * 2 + 3, self.order * 2 + 3), self.color["bc"])
         
         self._fill_color()
         
@@ -67,19 +72,115 @@ class Cube:
         self.ax = plt.figure().add_subplot(projection = '3d')
     
     def _fill_color(self):
-        self.facecolors[:, :, -1] = self.faces_up    # up:    yellow
-        self.facecolors[:, :, 0]  = self.faces_down  # down:  white
-        self.facecolors[:, 0, :]  = self.faces_right # right: orange
-        self.facecolors[:, -1, :] = self.faces_left  # left:  red
-        self.facecolors[0, :, :]  = self.faces_front # front: blue
-        self.facecolors[-1, :, :] = self.faces_down  # back:  green
+        x = 0
+        y = 0
+        for i in range(self.order * 2 + 3 - 1):
+            x = i // 2 - 1
+            for j in range(self.order * 2 + 3 - 1):
+                y = j // 2 - 1
+                if self.face_up[x, y] == self.face_color["y"]:
+                    self.colors_up[i, j] = self.color["y"]
+                elif self.face_up[x, y] == self.face_color["w"]:
+                    self.colors_up[i, j] = self.color["w"]
+                elif self.face_up[x, y] == self.face_color["r"]:
+                    self.colors_up[i, j] = self.color["r"]
+                elif self.face_up[x, y] == self.face_color["o"]:
+                    self.colors_up[i, j] = self.color["o"]
+                elif self.face_up[x, y] == self.face_color["b"]:
+                    self.colors_up[i, j] = self.color["b"]
+                elif self.face_up[x, y] == self.face_color["g"]:
+                    self.colors_up[i, j] = self.color["g"]
+                else:
+                    return
+                    
+                if self.face_down[x, y] == self.face_color["y"]:
+                    self.colors_down[i, j] = self.color["y"]
+                elif self.face_down[x, y] == self.face_color["w"]:
+                    self.colors_down[i, j] = self.color["w"]
+                elif self.face_down[x, y] == self.face_color["r"]:
+                    self.colors_down[i, j] = self.color["r"]
+                elif self.face_down[x, y] == self.face_color["o"]:
+                    self.colors_down[i, j] = self.color["o"]
+                elif self.face_down[x, y] == self.face_color["b"]:
+                    self.colors_down[i, j] = self.color["b"]
+                elif self.face_down[x, y] == self.face_color["g"]:
+                    self.colors_down[i, j] = self.color["g"]
+                else:
+                    return
+                
+                if self.face_left[x, y] == self.face_color["y"]:
+                    self.colors_left[i, j] = self.color["y"]
+                elif self.face_left[x, y] == self.face_color["w"]:
+                    self.colors_left[i, j] = self.color["w"]
+                elif self.face_left[x, y] == self.face_color["r"]:
+                    self.colors_left[i, j] = self.color["r"]
+                elif self.face_left[x, y] == self.face_color["o"]:
+                    self.colors_left[i, j] = self.color["o"]
+                elif self.face_left[x, y] == self.face_color["b"]:
+                    self.colors_left[i, j] = self.color["b"]
+                elif self.face_left[x, y] == self.face_color["g"]:
+                    self.colors_left[i, j] = self.color["g"]
+                else:
+                    return
+                
+                if self.face_right[x, y] == self.face_color["y"]:
+                    self.colors_right[i, j] = self.color["y"]
+                elif self.face_right[x, y] == self.face_color["w"]:
+                    self.colors_right[i, j] = self.color["w"]
+                elif self.face_right[x, y] == self.face_color["r"]:
+                    self.colors_right[i, j] = self.color["r"]
+                elif self.face_right[x, y] == self.face_color["o"]:
+                    self.colors_right[i, j] = self.color["o"]
+                elif self.face_right[x, y] == self.face_color["b"]:
+                    self.colors_right[i, j] = self.color["b"]
+                elif self.face_right[x, y] == self.face_color["g"]:
+                    self.colors_right[i, j] = self.color["g"]
+                else:
+                    return
+
+                if self.face_front[x, y] == self.face_color["y"]:
+                    self.colors_front[i, j] = self.color["y"]
+                elif self.face_front[x, y] == self.face_color["w"]:
+                    self.colors_front[i, j] = self.color["w"]
+                elif self.face_front[x, y] == self.face_color["r"]:
+                    self.colors_front[i, j] = self.color["r"]
+                elif self.face_front[x, y] == self.face_color["o"]:
+                    self.colors_front[i, j] = self.color["o"]
+                elif self.face_front[x, y] == self.face_color["b"]:
+                    self.colors_front[i, j] = self.color["b"]
+                elif self.face_front[x, y] == self.face_color["g"]:
+                    self.colors_front[i, j] = self.color["g"]
+                else:
+                    return
+                
+                if self.face_back[x, y] == self.face_color["y"]:
+                    self.colors_back[i, j] = self.color["y"]
+                elif self.face_back[x, y] == self.face_color["w"]:
+                    self.colors_back[i, j] = self.color["w"]
+                elif self.face_back[x, y] == self.face_color["r"]:
+                    self.colors_back[i, j] = self.color["r"]
+                elif self.face_back[x, y] == self.face_color["o"]:
+                    self.colors_back[i, j] = self.color["o"]
+                elif self.face_back[x, y] == self.face_color["b"]:
+                    self.colors_back[i, j] = self.color["b"]
+                elif self.face_back[x, y] == self.face_color["g"]:
+                    self.colors_back[i, j] = self.color["g"]
+                else:
+                    return
+                
+        self.facecolors[:, :, -1] = self.colors_up    # y
+        self.facecolors[:, :, 0]  = self.colors_down  # w
+        self.facecolors[:, 0, :]  = self.colors_right # o
+        self.facecolors[:, -1, :] = self.colors_left  # r
+        self.facecolors[0, :, :]  = self.colors_front # b
+        self.facecolors[-1, :, :] = self.colors_back  # g
         
     def _show(self):
         self.ax.voxels(self.x, self.y, self.z, self.body, facecolors = self.facecolors)
-        plt.ioff
+        plt.ioff()
         plt.axis("off")
         plt.show()
         
 if __name__ == '__main__':
-    cube = Cube(2)
+    cube = Cube(3)
     cube._show()
